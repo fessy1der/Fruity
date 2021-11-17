77b7d73fb345c45e1c65c06bd14b5f891156135f
Festus obi <fessy_wonder@yahoo.co.uk>
2021-11-17 10:23:09 +0100

    Initial Commit

---
+++ Fruity/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fruity
+//
+//  Created by Festus Obi on 17/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fruity/FruityApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruityApp.swift
+//  Fruity
+//
+//  Created by Festus Obi on 17/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct FruityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
